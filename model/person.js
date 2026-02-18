a193160b0a20fce24621cdd4266a9b896d4a1c81
KMIS2008 <iryna.makarenko@charityhero.live>
2026-02-18 22:07:28 +0200

    correct model and schema

--- model/person.js
+++ model/person.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
@@ -51,62 +50,15 @@ const personSchema = new Schema({
     }
   },
   
-  photo: {
-    type: String,
-    default: null
-  },
-  
   comments: {
     type: String,
     maxlength: [1000, 'Коментар не може перевищувати 1000 символів'],
     default: ''
-  },
-  
-  // ========================================
-  // ЗВ'ЯЗКИ З РОДИЧАМИ
-  // ========================================
-  
-  // Батьки (максимум 2)
-  parents: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Person'
-  }],
-  
-  // Подружжя (може бути декілька, якщо були розлучення)
-  spouses: [{
-    personId: {
-      type: Schema.Types.ObjectId,
-      ref: 'Person',
-      required: true
-    },
-    marriageDate: {
-      type: Date,
-      default: Date.now
-    },
-    divorceDate: {
-      type: Date,
-      default: null
-    }
-  }],
-  
-  // Діти (автоматично заповнюється при створенні дитини)
-  children: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Person'
-  }]
+  }
   
 }, {
   timestamps: true  // автоматично додає createdAt та updatedAt
 });
-
-// ========================================
-// ВАЛІДАЦІЯ НА РІВНІ СХЕМИ
-// ========================================
-
-// Перевірка: максимум 2 батьки
-personSchema.path('parents').validate(function(value) {
-  return value.length <= 2;
-}, 'Особа може мати максимум 2 батьків');
 
 // ========================================
 // МЕТОДИ МОДЕЛІ
@@ -129,80 +81,307 @@ personSchema.methods.getAge = function() {
   
   const endDate = this.deathDate || new Date();
   const age = endDate.getFullYear() - this.birthDate.getFullYear();
+  const monthDiff = endDate.getMonth() - this.birthDate.getMonth();
+  
+  // Корректировка возраста, если день рождения ещё не наступил в этом году
+  if (monthDiff < 0 || (monthDiff === 0 && endDate.getDate() < this.birthDate.getDate())) {
+    return age - 1;
+  }
   
   return age;
 };
 
-// Метод для отримання поточного подружжя
-personSchema.methods.getCurrentSpouse = async function() {
-  const currentMarriage = this.spouses.find(s => !s.divorceDate);
+// Метод для отримання інформації про життя
+personSchema.methods.getLifeInfo = function() {
+  const birthYear = this.birthDate ? this.birthDate.getFullYear() : null;
+  const deathYear = this.deathDate ? this.deathDate.getFullYear() : null;
   
-  if (!currentMarriage) return null;
+  if (birthYear && deathYear) {
+    return `${birthYear} - ${deathYear}`;
+  } else if (birthYear) {
+    return `${birthYear} - досі живий`;
+  }
   
-  return await mongoose.model('Person').findById(currentMarriage.personId);
+  return 'Дата народження невідома';
 };
 
 // ========================================
 // СТАТИЧНІ МЕТОДИ
 // ========================================
 
-// Знайти всіх нащадків особи
-personSchema.statics.findDescendants = async function(personId) {
-  const person = await this.findById(personId).populate('children');
-  if (!person) return [];
-  
-  let descendants = [...person.children];
-  
-  for (const child of person.children) {
-    const childDescendants = await this.findDescendants(child._id);
-    descendants = descendants.concat(childDescendants);
-  }
-  
-  return descendants;
+// Знайти всіх людей за статтю
+personSchema.statics.findByGender = function(gender) {
+  return this.find({ gender });
 };
 
-// Знайти всіх предків особи
-personSchema.statics.findAncestors = async function(personId) {
-  const person = await this.findById(personId).populate('parents');
-  if (!person || person.parents.length === 0) return [];
+// Знайти всіх живих людей
+personSchema.statics.findAlive = function() {
+  return this.find({ deathDate: null });
+};
+
+// Знайти померлих людей
+personSchema.statics.findDeceased = function() {
+  return this.find({ deathDate: { $ne: null } });
+};
+
+// Пошук за повним іменем
+personSchema.statics.searchByName = function(searchTerm) {
+  const regex = new RegExp(searchTerm, 'i'); // регістронезалежний пошук
   
-  let ancestors = [...person.parents];
-  
-  for (const parent of person.parents) {
-    const parentAncestors = await this.findAncestors(parent._id);
-    ancestors = ancestors.concat(parentAncestors);
-  }
-  
-  return ancestors;
+  return this.find({
+    $or: [
+      { firstName: regex },
+      { lastName: regex },
+      { middleName: regex }
+    ]
+  });
 };
 
 // ========================================
-// HOOKS (middleware)
+// ВІРТУАЛЬНІ ПОЛЯ
 // ========================================
 
-// Перед збереженням: перевіряємо циклічні зв'язки
-personSchema.pre('save', async function(next) {
-  if (this.isNew || this.isModified('parents')) {
-    // Перевіряємо, що особа не є своїм власним предком
-    if (this.parents.includes(this._id)) {
-      throw new Error('Особа не може бути власним батьком/матір\'ю');
-    }
-  }
-  
-  next();
+// Віртуальне поле для повного імені
+personSchema.virtual('fullName').get(function() {
+  return this.getFullName();
 });
+
+// Віртуальне поле для віку
+personSchema.virtual('age').get(function() {
+  return this.getAge();
+});
+
+// Увімкнути віртуальні поля в JSON
+personSchema.set('toJSON', { virtuals: true });
+personSchema.set('toObject', { virtuals: true });
 
 // ========================================
 // ІНДЕКСИ ДЛЯ ОПТИМІЗАЦІЇ ЗАПИТІВ
 // ========================================
 
-personSchema.index({ parents: 1 });
-personSchema.index({ 'spouses.personId': 1 });
-personSchema.index({ children: 1 });
+personSchema.index({ lastName: 1, firstName: 1 });
 personSchema.index({ gender: 1 });
 personSchema.index({ birthDate: 1 });
-personSchema.index({ lastName: 1, firstName: 1 });
+personSchema.index({ deathDate: 1 });
+
+// Текстовий індекс для пошуку
+personSchema.index({
+  firstName: 'text',
+  lastName: 'text',
+  middleName: 'text'
+});
 
 const Person = model('Person', personSchema);
 
 module.exports = Person;
+
+
+
+
+// const mongoose = require('mongoose');
+// const { Schema, model } = mongoose;
+
+// const personSchema = new Schema({
+//   firstName: {
+//     type: String,
+//     trim: true,
+//     default: ''
+//   },
+  
+//   lastName: {
+//     type: String,
+//     trim: true,
+//     default: ''
+//   },
+  
+//   middleName: {
+//     type: String,
+//     trim: true,
+//     default: ''
+//   },
+  
+//   gender: {
+//     type: String,
+//     enum: ['male', 'female'],
+//     required: [true, 'Стать є обов\'язковою']
+//   },
+  
+//   birthDate: {
+//     type: Date,
+//     default: null,
+//     validate: {
+//       validator: function(value) {
+//         if (!value) return true;
+//         return value <= new Date();
+//       },
+//       message: 'Дата народження не може бути в майбутньому'
+//     }
+//   },
+  
+//   deathDate: {
+//     type: Date,
+//     default: null,
+//     validate: {
+//       validator: function(value) {
+//         if (!value || !this.birthDate) return true;
+//         return value > this.birthDate;
+//       },
+//       message: 'Дата смерті не може бути раніше дати народження'
+//     }
+//   },
+  
+//   photo: {
+//     type: String,
+//     default: null
+//   },
+  
+//   comments: {
+//     type: String,
+//     maxlength: [1000, 'Коментар не може перевищувати 1000 символів'],
+//     default: ''
+//   },
+  
+//   // ========================================
+//   // ЗВ'ЯЗКИ З РОДИЧАМИ
+//   // ========================================
+  
+//   // Батьки (максимум 2)
+//   parents: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Person'
+//   }],
+  
+//   // Подружжя (може бути декілька, якщо були розлучення)
+//   spouses: [{
+//     personId: {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Person',
+//       required: true
+//     },
+//     marriageDate: {
+//       type: Date,
+//       default: Date.now
+//     },
+//     divorceDate: {
+//       type: Date,
+//       default: null
+//     }
+//   }],
+  
+//   // Діти (автоматично заповнюється при створенні дитини)
+//   children: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Person'
+//   }]
+  
+// }, {
+//   timestamps: true  // автоматично додає createdAt та updatedAt
+// });
+
+// // ========================================
+// // ВАЛІДАЦІЯ НА РІВНІ СХЕМИ
+// // ========================================
+
+// // Перевірка: максимум 2 батьки
+// personSchema.path('parents').validate(function(value) {
+//   return value.length <= 2;
+// }, 'Особа може мати максимум 2 батьків');
+
+// // ========================================
+// // МЕТОДИ МОДЕЛІ
+// // ========================================
+
+// // Метод для отримання повного імені
+// personSchema.methods.getFullName = function() {
+//   const parts = [this.lastName, this.firstName, this.middleName].filter(Boolean);
+//   return parts.join(' ') || 'Без імені';
+// };
+
+// // Метод для перевірки, чи жива особа
+// personSchema.methods.isAlive = function() {
+//   return !this.deathDate;
+// };
+
+// // Метод для отримання віку
+// personSchema.methods.getAge = function() {
+//   if (!this.birthDate) return null;
+  
+//   const endDate = this.deathDate || new Date();
+//   const age = endDate.getFullYear() - this.birthDate.getFullYear();
+  
+//   return age;
+// };
+
+// // Метод для отримання поточного подружжя
+// personSchema.methods.getCurrentSpouse = async function() {
+//   const currentMarriage = this.spouses.find(s => !s.divorceDate);
+  
+//   if (!currentMarriage) return null;
+  
+//   return await mongoose.model('Person').findById(currentMarriage.personId);
+// };
+
+// // ========================================
+// // СТАТИЧНІ МЕТОДИ
+// // ========================================
+
+// // Знайти всіх нащадків особи
+// personSchema.statics.findDescendants = async function(personId) {
+//   const person = await this.findById(personId).populate('children');
+//   if (!person) return [];
+  
+//   let descendants = [...person.children];
+  
+//   for (const child of person.children) {
+//     const childDescendants = await this.findDescendants(child._id);
+//     descendants = descendants.concat(childDescendants);
+//   }
+  
+//   return descendants;
+// };
+
+// // Знайти всіх предків особи
+// personSchema.statics.findAncestors = async function(personId) {
+//   const person = await this.findById(personId).populate('parents');
+//   if (!person || person.parents.length === 0) return [];
+  
+//   let ancestors = [...person.parents];
+  
+//   for (const parent of person.parents) {
+//     const parentAncestors = await this.findAncestors(parent._id);
+//     ancestors = ancestors.concat(parentAncestors);
+//   }
+  
+//   return ancestors;
+// };
+
+// // ========================================
+// // HOOKS (middleware)
+// // ========================================
+
+// // Перед збереженням: перевіряємо циклічні зв'язки
+// personSchema.pre('save', async function(next) {
+//   if (this.isNew || this.isModified('parents')) {
+//     // Перевіряємо, що особа не є своїм власним предком
+//     if (this.parents.includes(this._id)) {
+//       throw new Error('Особа не може бути власним батьком/матір\'ю');
+//     }
+//   }
+  
+//   next();
+// });
+
+// // ========================================
+// // ІНДЕКСИ ДЛЯ ОПТИМІЗАЦІЇ ЗАПИТІВ
+// // ========================================
+
+// personSchema.index({ parents: 1 });
+// personSchema.index({ 'spouses.personId': 1 });
+// personSchema.index({ children: 1 });
+// personSchema.index({ gender: 1 });
+// personSchema.index({ birthDate: 1 });
+// personSchema.index({ lastName: 1, firstName: 1 });
+
+// const Person = model('Person', personSchema);
+
+// module.exports = Person;
--- schemas/personSchema.js
+++ schemas/personSchema.js
@@ -4,16 +4,22 @@ const Joi = require('joi');
 const personSchema = Joi.object({
   firstName: Joi.string()
     .trim()
+    .min(1)
+    .max(50)
     .optional()
     .allow(''),
   
   lastName: Joi.string()
     .trim()
+    .min(1)
+    .max(50)
     .optional()
     .allow(''),
   
   middleName: Joi.string()
     .trim()
+    .min(1)
+    .max(50)
     .optional()
     .allow(''),
   
@@ -41,33 +47,6 @@ const personSchema = Joi.object({
       'date.greater': 'Дата смерті не може бути раніше дати народження'
     }),
   
-  // Зв'язки з батьками
-  parent1: Joi.string()
-    .optional()
-    .allow(null, ''),
-  
-  parent2: Joi.string()
-    .optional()
-    .allow(null, '')
-    .when('parent1', {
-      is: Joi.exist(),
-      then: Joi.string().invalid(Joi.ref('parent1'))
-        .messages({
-          'any.invalid': 'Батько/мати 2 не може бути тим самим, що й батько/мати 1'
-        })
-    }),
-  
-  // Зв'язок з подружжям
-  spouse: Joi.string()
-    .optional()
-    .allow(null, ''),
-  
-  // Фото (валідація на фронтенді, тут перевіряємо URL або шлях)
-  photo: Joi.string()
-    .uri()
-    .optional()
-    .allow(null, ''),
-  
   comments: Joi.string()
     .max(1000)
     .optional()
@@ -77,39 +56,123 @@ const personSchema = Joi.object({
     })
 });
 
-// Схема для створення шлюбу між двома людьми
-const marriageSchema = Joi.object({
-  person1Id: Joi.string()
-    .required()
-    .messages({
-      'any.required': 'ID першої особи обов\'язкове'
-    }),
-  
-  person2Id: Joi.string()
-    .required()
-    .invalid(Joi.ref('person1Id'))
-    .messages({
-      'any.required': 'ID другої особи обов\'язкове',
-      'any.invalid': 'Особа не може одружитися сама з собою',
-    }),
-  
-  marriageDate: Joi.date()
-    .max('now')
-    .optional()
-    .messages({
-      'date.max': 'Дата шлюбу не може бути в майбутньому'
-    }),
-  
-  divorceDate: Joi.date()
-    .greater(Joi.ref('marriageDate'))
-    .optional()
-    .allow(null)
-    .messages({
-      'date.greater': 'Дата розлучення не може бути раніше дати шлюбу'
-    })
-});
-
 module.exports = {
-  personSchema,
-  marriageSchema
+  personSchema
 };
+
+
+// const Joi = require('joi');
+
+// // Схема для создания/обновления персоны
+// const personSchema = Joi.object({
+//   firstName: Joi.string()
+//     .trim()
+//     .optional()
+//     .allow(''),
+  
+//   lastName: Joi.string()
+//     .trim()
+//     .optional()
+//     .allow(''),
+  
+//   middleName: Joi.string()
+//     .trim()
+//     .optional()
+//     .allow(''),
+  
+//   gender: Joi.string()
+//     .valid('male', 'female')
+//     .required()
+//     .messages({
+//       'any.required': 'Стать є обов\'язковою',
+//       'any.only': 'Стать має бути "male" або "female"'
+//     }),
+  
+//   birthDate: Joi.date()
+//     .max('now')
+//     .optional()
+//     .allow(null, '')
+//     .messages({
+//       'date.max': 'Дата народження не може бути в майбутньому'
+//     }),
+  
+//   deathDate: Joi.date()
+//     .greater(Joi.ref('birthDate'))
+//     .optional()
+//     .allow(null, '')
+//     .messages({
+//       'date.greater': 'Дата смерті не може бути раніше дати народження'
+//     }),
+  
+//   // Зв'язки з батьками
+//   parent1: Joi.string()
+//     .optional()
+//     .allow(null, ''),
+  
+//   parent2: Joi.string()
+//     .optional()
+//     .allow(null, '')
+//     .when('parent1', {
+//       is: Joi.exist(),
+//       then: Joi.string().invalid(Joi.ref('parent1'))
+//         .messages({
+//           'any.invalid': 'Батько/мати 2 не може бути тим самим, що й батько/мати 1'
+//         })
+//     }),
+  
+//   // Зв'язок з подружжям
+//   spouse: Joi.string()
+//     .optional()
+//     .allow(null, ''),
+  
+//   // Фото (валідація на фронтенді, тут перевіряємо URL або шлях)
+//   photo: Joi.string()
+//     .uri()
+//     .optional()
+//     .allow(null, ''),
+  
+//   comments: Joi.string()
+//     .max(1000)
+//     .optional()
+//     .allow('', null)
+//     .messages({
+//       'string.max': 'Коментар не може перевищувати 1000 символів'
+//     })
+// });
+
+// // Схема для створення шлюбу між двома людьми
+// const marriageSchema = Joi.object({
+//   person1Id: Joi.string()
+//     .required()
+//     .messages({
+//       'any.required': 'ID першої особи обов\'язкове'
+//     }),
+  
+//   person2Id: Joi.string()
+//     .required()
+//     .invalid(Joi.ref('person1Id'))
+//     .messages({
+//       'any.required': 'ID другої особи обов\'язкове',
+//       'any.invalid': 'Особа не може одружитися сама з собою',
+//     }),
+  
+//   marriageDate: Joi.date()
+//     .max('now')
+//     .optional()
+//     .messages({
+//       'date.max': 'Дата шлюбу не може бути в майбутньому'
+//     }),
+  
+//   divorceDate: Joi.date()
+//     .greater(Joi.ref('marriageDate'))
+//     .optional()
+//     .allow(null)
+//     .messages({
+//       'date.greater': 'Дата розлучення не може бути раніше дати шлюбу'
+//     })
+// });
+
+// module.exports = {
+//   personSchema,
+//   marriageSchema
+// };
